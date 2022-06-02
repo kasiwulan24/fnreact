7681bc65dc807e81b3348c730756d4dfc94f5379
Andi Kasiwulan <61861878+kasiwulan24@users.noreply.github.com>
2022-06-02 10:42:23 +0700

    first commit

---
+++ src/components/Button/index.test.jsx
@@ -0,0 +1,16 @@
+// import React from 'react'
+// import { render,} from '@testing-library/react'
+// import Button from './index'
+
+// test("Should not allowed button if isDisabled is present", ()=> {
+//     // const {container} = render(<Button isDisabled></Button>)
+//     // expect(container.querySelector("span.disabled")).toBeInTheDocument()
+
+//     render (<Button/>)
+// });
+
+// test("Should render loading/spinner",() =>{
+//     const {container, getByText} = render (<Button isLoading></Button>);
+
+//     expect (container.querySelector("span")).toBeInTheDocument();
+// })
